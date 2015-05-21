c3479e4ed44b95a95b779832c07a613541cad88f
Tom Lee <tom_t_lee@yahoo.com>
2015-05-20 20:47:56 -0400

    Continue on locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,16 +2,17 @@ class LocationsController < ApplicationController
   def new
     @location = Location.new
   end
-  
+
   def create
     @location = Location.create(location_params)
+    redirect_to home_path
   end
-  
+
   def index
     @location = Location.new
     @locations = Location.all
   end
-  
+
   private
     def location_params
       params.require(:location).permit(:address)
