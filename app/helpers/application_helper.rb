41663414110bdcbbb72da5ee6978cd4288406220
Tom Lee <tom_t_lee@yahoo.com>
2015-05-25 15:13:48 -0400

    Refactor map links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,10 @@ module ApplicationHelper
     end
 
     markers_string = markers_array.join
-    "http://maps.google.com/maps/api/staticmap?size=600x400&sensor=false#{markers_string}"
+    "http://maps.google.com/maps/api/staticmap?size=640x640#{markers_string}"
+  end
+
+  def single_location_map_link
+    "http://maps.google.com/maps/api/staticmap?size=640x480&sensor=false&zoom=16&markers=#{@location.address}"
   end
 end
