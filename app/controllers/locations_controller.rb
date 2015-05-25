200b38e2fd6aee33bab200d981578068fa395ae8
Tom Lee <tom_t_lee@yahoo.com>
2015-05-25 14:27:49 -0400

    Refactor locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,7 @@
 class LocationsController < ApplicationController
   before_action :require_user
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
+  before_action :require_location_creator, only: [:edit, :update, :destroy]
 
   def new
     @location = Location.new
@@ -21,49 +23,29 @@ class LocationsController < ApplicationController
   def index
     @location = Location.new
     @locations = current_user.locations
-
-    markers_array = []
-
-    @locations.each do |location|
-      marker = "&markers=label:#{location.id}%7C#{location.latitude},#{location.longitude}"
-      markers_array << marker
-    end
-
-    @markers = markers_array.join
   end
 
   def show
-    @location = Location.find(params[:id])
   end
 
   def edit
-    @location = Location.find(params[:id])
-    require_location_creator(@location)
   end
 
   def update
-    @location = Location.find(params[:id])
     @location.address = location_params[:address]
 
-    if (@location.user == current_user) && @location.save
+    if @location.save
       flash[:success] = "The address has been updated successfully!"
       redirect_to location_path(@location)
-    elsif @location.user == current_user
+    else
       flash[:error] = "Please fix the following errors."
       render :edit
-    else
-      flash[:error] = "You do not have permission to do that."
-      redirect_to home_path
     end
   end
 
   def destroy
-    @location = Location.find(params[:id])
-    if (@location.user == current_user) && @location.destroy
+    if @location.destroy
       flash[:notice] = "Address has been deleted successfully"
-      redirect_to home_path
-    elsif @location.user != current_user
-      flash[:error] = "You do not have permission to do that."
       redirect_to home_path
     end
   end
@@ -73,9 +55,13 @@ class LocationsController < ApplicationController
       params.require(:location).permit(:address)
     end
 
-    def require_location_creator(location)
-      if location.user != current_user
-        flash[:error] = "You do not have permission to do that."
+    def set_location
+      @location = Location.find(params[:id])
+    end
+
+    def require_location_creator
+      unless current_user == @location.user
+        flash[:error] = "You are not allowed to do that."
         redirect_to home_path
       end
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def index_map_link
+    markers_array = []
+
+    @locations.each do |location|
+      markers_array << "&markers=#{location.latitude},#{location.longitude}"
+    end
+
+    markers_string = markers_array.join
+    "http://maps.google.com/maps/api/staticmap?size=600x400&sensor=false#{markers_string}"
+  end
 end
